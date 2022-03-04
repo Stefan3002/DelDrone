0983364bf841f17dbf2e7b7f944dcdb7b771ca54
Stefan3002 <92794548+Stefan3002@users.noreply.github.com>
2022-03-04 17:00:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function SwitchDarkMode(){
+function SwitchModeHelper(){
+    const aux = document.querySelector("i")
     const body = document.querySelector("body")
+    if(!body.classList.contains("darkStyle")) {
+        aux.classList.replace("fa-moon","fa-sun")
+        SwitchDarkMode(body)
+    }
+    else {
+        SwitchLightMode(body)
+        aux.classList.replace("fa-sun","fa-moon")
+    }
+}
+
+function SwitchDarkMode(body){
+    // const body = document.querySelector("body")
     body.classList.toggle("darkStyle")
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach(button => SwitchDarkModeButton(button))
+}
+function SwitchDarkModeButton(button){
+    button.classList.replace("btn-outline-dark","btn-outline-light")
+}
+
+
+function SwitchLightMode(body){
+    // const body = document.querySelector("body")
+    body.classList.remove("darkStyle")
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach(button => SwitchLightModeButton(button))
+}
+function SwitchLightModeButton(button){
+    button.classList.replace("btn-outline-light","btn-outline-dark")
 }
